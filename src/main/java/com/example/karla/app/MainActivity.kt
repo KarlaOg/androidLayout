8b3ca543100503efe6af6977826128a08e9452aa
KarlaOg <33722964+KarlaOg@users.noreply.github.com>
2018-12-09 16:46:16 +0100

    First Commit

---
+++ src/main/java/com/example/karla/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.karla.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setContentView(R.layout.second)
+
+    }
+}
